6fa9d3dc82488d71d97276e1a56667600f9edeb5
Jason Prillo <jasonprillo@gmail.com>
2022-07-29 21:12:34 -0400

    add blog to cms

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,10 +1,21 @@
 import React from "react"
 import Layout from '../../components/layout2'
+import { graphql } from "gatsby"
+import {Link} from "gatsby"
+
 
 import { Helmet } from 'react-helmet'
 
-export default function Blog() {
-  
+
+export default function Blog({data}) {
+    const d = data.allMarkdownRemark;
+    const h = d.edges[0].node.frontmatter
+    const hslug = d.edges[0].node.fields.slug
+    const f = d.edges[1].node.frontmatter
+    const fslug = d.edges[1].node.fields.slug
+    const t = d.edges[2].node.frontmatter
+    const tslug = d.edges[2].node.fields.slug
+    
   return <div >
       <Helmet>
         <title>Geeby | Gatsby Starter Blog</title>
@@ -14,22 +25,53 @@ export default function Blog() {
       <Layout>
 <div className="flex gap-2 pad-top ">
     <div className="col-8">
-        <div className="feature-article" style={{background: "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('https://blog.logrocket.com/wp-content/uploads/2019/05/logrocket-blog.jpg')"}}>
-            <h2>This is the first blog article</h2>
-            <p>This is a good plce for a description</p>
+        <Link to = {hslug}>
+        <div className="feature-article" style={{background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('${h.featuredImage.publicURL}')`}}>
+        
+            <h2>{h.title}</h2>
+            <p>{h.description}</p>
         </div>
+        </Link>
     </div>
     <div className="col-4 column gap-2">
-    <div className="article" style={{backgroundImage: "url(https://blog.logrocket.com/wp-content/uploads/2019/05/logrocket-blog.jpg)"}}>
-            <h3>This is the first blog article</h3>
-            <p>This is a good plce for a description</p>
+        <Link to ={tslug}>
+    <div className="article" style={{background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('${f.featuredImage.publicURL}')`}}>
+            <h3>{f.title}</h3>
+            <p>{f.description}</p>
         </div>
-        <div className="article" style={{backgroundImage: "url(https://blog.logrocket.com/wp-content/uploads/2019/05/logrocket-blog.jpg)"}}>
-            <h3>This is the first blog article</h3>
-            <p>This is a good plce for a description</p>
+        </Link>
+
+        <Link to ={fslug}>
+        <div className="article" style={{background: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('${t.featuredImage.publicURL}')`}}>
+        <h3>{t.title}</h3>
+            <p>{t.description}</p>
         </div>
+        </Link>
     </div>
 </div>
   </Layout>
   </div>
 }
+
+
+export const query = graphql`
+query MyQuery {
+    allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "blog-post"}}}) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            description
+            featuredImage {
+                publicURL
+             }
+          }
+        }
+      }
+    }
+  }
+`
