dc179670407520fad948b70c339d9c683000b51d
Jason Prillo <jasonprillo@gmail.com>
2022-07-30 21:35:18 -0400

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,8 @@
 import CMS from 'netlify-cms-app'
+import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
+CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
